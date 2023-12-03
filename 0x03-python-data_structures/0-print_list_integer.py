b896b1683049f41d466679c39add9acbe203b789
Nader-albaire <nader.albaire@gmail.com>
2023-12-03 05:28:56 -0800

    last Edit

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-<<<<<<< Updated upstream
-        for num in range(len(my_list)):
+    for num in range(len(my_list)):
         print("{:d}".format(my_list[num]))
-=======
-<<<<<<< HEAD
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
-=======
-        for num in range(len(my_list)):
-        print("{:d}".format(my_list[num]))
->>>>>>> a938c714a72e2c48522510d8353359d38ef44521
->>>>>>> Stashed changes
