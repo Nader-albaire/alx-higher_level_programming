1389c1b0d987f0349ca5b6bd8af41c6b6db58c9f
Nader-albaire <140080327+Nader-albaire@users.noreply.github.com>
2023-12-03 16:22:37 +0300

    update

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,19 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+    else:
+        my_list[idx] = element
+        return my_list
+=======
+>>>>>>> Stashed changes
         else:
             my_list[idx] = element
             return my_list
 
+<<<<<<< Updated upstream
+=======
+>>>>>>> a938c714a72e2c48522510d8353359d38ef44521
+>>>>>>> Stashed changes
