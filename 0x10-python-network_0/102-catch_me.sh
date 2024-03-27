28557930047e89e41bfdbc05644a81a656452b3b
Nader-albaire <nader.albaire@gmail.com>
2024-03-27 11:46:02 -0700

    New file

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# This script makes a request to 0.0.0.0:5000/catch_me and causes the server to respond with a message containing "You got me!"
-
-# Send a PUT request with custom data to trigger the desired response
-curl -s -X PUT --data "user_id=98" 0.0.0.0:5000/catch_me
-
+# Makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
+curl -sL -X PUT -H "Origin: X-School" -d "user_id=98" 0.0.0.0:5000/catch_me
